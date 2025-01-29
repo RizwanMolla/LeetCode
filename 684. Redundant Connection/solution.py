42b38219d7db64b5f40f69105cb84066bcddb7ab
MD RIZWAN MOLLA <rizwanmolla711410@gmail.com>
2025-01-29 18:09:18 +0530

    DCC 29-01-2025

---
+++ 684. Redundant Connection/solution.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findRedundantConnection(self, edges: list[list[int]]) -> list[int]:
+        parent = [i for i in range(len(edges) + 1)]
+        rank = [1] * (len(edges) + 1)
+
+        def find(u):
+            while parent[u] != u:
+                parent[u] = parent[parent[u]]
+                u = parent[u]
+            return u
+
+        def union(u, v):
+            u_root = find(u)
+            v_root = find(v)
+            if u_root == v_root:
+                return False
+            if rank[u_root] > rank[v_root]:
+                parent[v_root] = u_root
+            elif rank[u_root] < rank[v_root]:
+                parent[u_root] = v_root
+            else:
+                parent[v_root] = u_root
+                rank[u_root] += 1
+            return True
+
+        for u, v in edges:
+            if not union(u, v):
+                return [u, v]
+        return []
