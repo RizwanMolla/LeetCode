708e143fb9cdb866c2f20929b5a14ed10fd93aae
MD RIZWAN MOLLA <rizwanmolla711410@gmail.com>
2025-02-13 00:48:36 +0530

    DCC 04-12-2024

---
+++ 2825. Make String a Subsequence Using Cyclic Increments/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def canMakeSubsequence(self, str1: str, str2: str) -> bool:
+        i, j = 0, 0
+        n, m = len(str1), len(str2)
+        
+        while i < n and j < m:
+            if str1[i] == str2[j] or chr((ord(str1[i]) - ord('a') + 1) % 26 + ord('a')) == str2[j]:
+                j += 1
+            i += 1
+        
+        return j == m
