4074320ed20b2fccd3efbd130179a9db86b70aa2
MD RIZWAN MOLLA <rizwanmolla711410@gmail.com>
2025-03-17 19:04:16 +0530

    DCC 17-03-2025

---
+++ 2206. Divide Array Into Equal Pairs/solution.py
@@ -0,0 +1,7 @@
+from collections import Counter
+from typing import List
+
+class Solution:
+    def divideArray(self, nums: List[int]) -> bool:
+        freq = Counter(nums)
+        return all(count % 2 == 0 for count in freq.values())
