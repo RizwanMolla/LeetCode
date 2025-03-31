208a9b501380df33dfefb8b831f6501798caee48
MD RIZWAN MOLLA <rizwanmolla711410@gmail.com>
2025-03-31 23:51:13 +0530

    DCC 31-03-2025

---
+++ 2551. Put Marbles in Bags/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def putMarbles(self, weights: list[int], k: int) -> int:
+        n = len(weights)
+        if k == 1:
+            return 0
+
+        pair_sums = [weights[i] + weights[i + 1] for i in range(n - 1)]
+        pair_sums.sort()
+
+        min_score = sum(pair_sums[:k - 1])
+        max_score = sum(pair_sums[-(k - 1):])
+
+        return max_score - min_score
