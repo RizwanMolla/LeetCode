c3ff649a04d621b2ce25e40cd4306c44242d3444
MD RIZWAN MOLLA <rizwanmolla711410@gmail.com>
2025-04-24 20:48:32 +0530

    DCC 24-04-2025

---
+++ 2799. Count Complete Subarrays in an Array/solution.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+
+class Solution:
+    def countCompleteSubarrays(self, nums: list[int]) -> int:
+        total_distinct = len(set(nums))
+        count = 0
+        n = len(nums)
+        
+        for i in range(n):
+            seen = set()
+            for j in range(i, n):
+                seen.add(nums[j])
+                if len(seen) == total_distinct:
+                    count += 1
+        return count
