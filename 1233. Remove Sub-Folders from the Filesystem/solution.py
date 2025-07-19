984934d36df33fe438bfa27ce4507904379de976
MD RIZWAN MOLLA <rizwanmolla711410@gmail.com>
2025-07-19 18:35:46 +0530

    DCC 19-07-2025

---
+++ 1233. Remove Sub-Folders from the Filesystem/solution.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def removeSubfolders(self, folder: List[str]) -> List[str]:
+        folder.sort() 
+        result = [folder[0]]
+
+        for path in folder[1:]:
+
+            if not path.startswith(result[-1] + "/"):
+                result.append(path)
+
+        return result
