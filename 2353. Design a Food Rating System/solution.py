f5760aca8d73856f4472edbcfd4842dadb3be087
MD RIZWAN MOLLA <rizwanmolla711410@gmail.com>
2025-09-17 23:33:56 +0530

    DCC 17-09-2025

---
+++ 2353. Design a Food Rating System/solution.py
@@ -0,0 +1,33 @@
+import heapq
+from typing import List
+
+class FoodRatings:
+
+    def __init__(self, foods: List[str], cuisines: List[str], ratings: List[int]):
+        # food -> cuisine
+        self.food_to_cuisine = {}
+        # food -> rating
+        self.food_to_rating = {}
+        # cuisine -> max-heap [(-rating, food)]
+        self.cuisine_to_heap = {}
+
+        for f, c, r in zip(foods, cuisines, ratings):
+            self.food_to_cuisine[f] = c
+            self.food_to_rating[f] = r
+            if c not in self.cuisine_to_heap:
+                self.cuisine_to_heap[c] = []
+            heapq.heappush(self.cuisine_to_heap[c], (-r, f))
+
+    def changeRating(self, food: str, newRating: int) -> None:
+        cuisine = self.food_to_cuisine[food]
+        self.food_to_rating[food] = newRating
+        heapq.heappush(self.cuisine_to_heap[cuisine], (-newRating, food))
+
+    def highestRated(self, cuisine: str) -> str:
+        heap = self.cuisine_to_heap[cuisine]
+        # lazy cleanup: discard outdated ratings
+        while heap:
+            rating, food = heap[0]
+            if -rating == self.food_to_rating[food]:
+                return food
+            heapq.heappop(heap)  # remove outdated entry
