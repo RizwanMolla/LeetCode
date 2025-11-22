315a39e6ad785a95f15198570cec82f905463c8b
MD RIZWAN MOLLA <rizwanmolla711410@gmail.com>
2025-11-22 10:50:21 +0530

    DCC 22-11-2025

---
+++ 3190. Find Minimum Operations to Make All Elements Divisible by Three/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        operations = 0
+        for x in nums:
+            if x % 3 != 0:
+                operations += 1
+        return operations
