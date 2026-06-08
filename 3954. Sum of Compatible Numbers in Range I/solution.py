150215937fc11c157c607060c7f7403d79a8c8ff
MD RIZWAN MOLLA <rizwanmolla711410@gmail.com>
2026-06-08 22:26:55 +0530

    Weekly Contest 505

---
+++ 3954. Sum of Compatible Numbers in Range I/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sumOfGoodIntegers(self, n: int, k: int) -> int:
+        left = max(1, n - k)
+        right = n + k
+
+        ans = 0
+        for x in range(left, right + 1):
+            if (n & x) == 0:
+                ans += x
+
+        return ans
